b4f5334f1212ae0824b49c60596071ebd3e9aa73
manikandan <manikandan.j@cgvakindia.com>
2024-09-27 22:29:00 +0530

    Add project files.

---
+++ Model/ToDoList.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppTask1.Model
+{
+    public class ToDoList
+    {
+        public required string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsEditing { get; set; }
+    }
+}
